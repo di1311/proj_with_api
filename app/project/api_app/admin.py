8b728ee2ec0c662812e31b446d9a5d25a2bdd168
Dmitry Sid <di280697@gmail.com>
2021-03-02 19:54:43 +0300

    5 commit

--- app/project/api_app/admin.py
+++ app/project/api_app/admin.py
@@ -1,2 +1,10 @@
 from django.contrib import admin
+from .models import Games
+
+
+admin.site.register(Games)
+
+
+
+
 
--- app/project/api_app/models.py
+++ app/project/api_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 
+class Games(models.Model):
+    title = models.CharField('Название игры', max_length=150)
+    cheapest_price = models.PositiveSmallIntegerField('Самая низкая цена', default=0)
+    store = models.CharField('Магазин', max_length=150)
+
+
+    class Meta:
+        verbose_name = 'Games'
+        verbose_name_plural = 'Games'
+
+
