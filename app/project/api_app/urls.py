9a7ac5241b1ca0b812d02768c305da3f28ff6b1a
Dmitry Sid <di280697@gmail.com>
2021-03-04 12:05:02 +0300

    В urlpatterns указал классы и их метод as_view()

--- app/project/api_app/urls.py
+++ app/project/api_app/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import *
 
 
+
 urlpatterns = [
     path('', ViewHomePage.as_view(), name='home_page_url'),
     path('search/', ViewResponse.as_view(), name='search_view_url'),
-    path('search/<str:id>_<str:deal_id>_<str:title>', GameDetailView.as_view(), name='game_detail_url'),
+    path('search/<str:id>_<str:deal_id>', GameDetailView.as_view(), name='game_detail_url'),
 ]
-
 
